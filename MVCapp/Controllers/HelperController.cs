db69d0a66a66e3311ab553df926692ea71a5c8e0
zietara.sadadamzian1 <zietara.dazzzmian1@gmail.com>
2018-07-30 17:22:36 +0200

    Połączenie z bazą danych ADO Net i wyświetlenie pól

--- MVCapp/Controllers/HelperController.cs
+++ MVCapp/Controllers/HelperController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,7 +12,30 @@ namespace MVCapp.Controllers
         // GET: Helper
         public ActionResult Index()
         {
-            return View();
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;"
+                +"AttachDbFilename=C:\\Users\\Damian\\source\\repos\\MVCapp\\MVCapp\\App_Data\\BAZAADONET.mdf;Integrated Security=True";
+
+            string query = "Select Nazwa, Email From dbo.KontaktyADO where id= @id";
+
+            int id = 2;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@id", id);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                reader.Read();
+
+                var nazwa = reader[0];
+                var email = reader[1];
+               
+                ViewBag.Nazwa = nazwa;
+                ViewBag.Email = email;
+            }
+                return View();
         }
     }
 }
