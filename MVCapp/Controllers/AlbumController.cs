f9b59fbe02de7e47779679b9777fa634e259556c
zietara.sadadamzian1 <zietara.dazzzmian1@gmail.com>
2018-08-02 15:00:45 +0200

    Ajax i biblioteka mustache

---
+++ MVCapp/Controllers/AlbumController.cs
@@ -0,0 +1,24 @@
+﻿using MVCapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCapp.Controllers
+{
+    public class AlbumController : Controller
+    {
+        // GET: Album
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Album album)
+        {
+            //return Content(string.Format("Tytuł: {0} , Autor: {1}, Cena: {2}",album.Tytul,album.Autor,album.Cena));
+            return Json(album);
+        }
+    }
+}
---
+++ MVCapp/Models/Album.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCapp.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+        public string Tytul { get; set; }
+        public string Autor { get; set; }
+        public double Cena { get; set; }
+    }
+}
