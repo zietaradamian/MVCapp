0a4156ae2a543c6bf496b6d51d0664e377daf49d
zietara.sadadamzian1 <zietara.dazzzmian1@gmail.com>
2018-08-02 16:03:29 +0200

    AJAX (AutoComplet) Progressive enhancement

---
+++ MVCapp/Controllers/OsobaController.cs
@@ -0,0 +1,35 @@
+﻿using MVCapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCapp.Controllers
+{
+    public class OsobaController : Controller
+    {
+        // GET: Osoba
+        public ActionResult Index(string szukaj = null)
+        {
+            IEnumerable<Osoba> listaOsob;
+            if (szukaj != null)
+            {
+                listaOsob = Osoba.PobierzListeOsob().Where(p => p.Imie.Contains(szukaj) || p.Nazwisko.Contains(szukaj) || szukaj == p.Imie + " " + p.Nazwisko).ToArray();
+            }
+            else
+                listaOsob = Osoba.PobierzListeOsob().ToArray();
+
+            if (Request.IsAjaxRequest())
+                return PartialView("_ListaOsob", listaOsob);       
+                    
+            return View(listaOsob);
+        }
+        public ActionResult OsobaPropozycje(string term)
+        {
+            var listaOsob = Osoba.PobierzListeOsob().Where(p => p.Imie.Contains(term) || p.Nazwisko.Contains(term))
+                .Take(4).Select(p=> new { label = p.Imie + " " + p.Nazwisko });
+            return Json(listaOsob, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ MVCapp/Models/Osoba.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCapp.Models
+{
+    public class Osoba
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+
+        public static IEnumerable<Osoba> PobierzListeOsob()
+        {
+            var osoby = new List<Osoba>
+            {
+                new Osoba() { Imie="Andrzej", Nazwisko="Maien"},
+                new Osoba() { Imie="Arek", Nazwisko="Nowak"},
+                new Osoba() { Imie="Amadeusz", Nazwisko="Maien"},
+                new Osoba() { Imie="Rysiu", Nazwisko="krowka"},
+                new Osoba() { Imie="Andrzej", Nazwisko="Maien"},
+                new Osoba() { Imie="Andrzej", Nazwisko="Maien"},
+                new Osoba() { Imie="Andrzej", Nazwisko="eryk"},
+                new Osoba() { Imie="WArrr", Nazwisko="Maien"},
+                new Osoba() { Imie="Andrzej", Nazwisko="Maien"},
+                new Osoba() { Imie="Warsza", Nazwisko="Wigra"},
+                new Osoba() { Imie="Aleksandy", Nazwisko="Maien"},
+                new Osoba() { Imie="Andrzej", Nazwisko="Maien"},
+            };
+
+            return osoby;
+        }
+    }   
+}
