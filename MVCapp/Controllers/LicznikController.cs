4dfaa14addad6875499e94044ecb8bce7325b9fb
zietara.sadadamzian1 <zietara.dazzzmian1@gmail.com>
2018-08-06 17:16:23 +0200

    zmienne statyczne licznik

---
+++ MVCapp/Controllers/LicznikController.cs
@@ -0,0 +1,37 @@
+﻿using MVCapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCapp.Controllers
+{
+    public class LicznikController : Controller
+    {
+        private object licznikStatycznyBlokada = new object();
+        // GET: Licznik
+        public ActionResult Index()
+        {
+            var licznik = PobierzLicznik();
+            return View(licznik);
+        }
+        [HttpPost]
+        public ActionResult LicznikStatyczny()
+        {
+            lock(licznikStatycznyBlokada)
+            {
+                var licznik = PobierzLicznik();
+                licznik.LicznikStatyczny++;
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        private Liczniki PobierzLicznik()
+        {
+            var licznik = new Liczniki();
+            return licznik;
+        }
+    }
+}
---
+++ MVCapp/Models/Liczniki.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCapp.Models
+{
+    public class Liczniki
+    {
+        private static int _licznikstatyczny;
+        public int LicznikStatyczny
+        {
+            get {
+                return Liczniki._licznikstatyczny;
+            }
+            set {
+                Liczniki._licznikstatyczny = value;
+            }
+        }
+    }
+}
